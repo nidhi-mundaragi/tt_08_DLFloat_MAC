02030f78ba53e9591fadd5f50a964c473008f4d4
Nidhi M D <166122007+nidhi-mundaragi@users.noreply.github.com>
2024-08-13 12:55:11 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -32,14 +32,25 @@ async def test_project(dut):
     # Set the input values b1=3
     dut.ui_in.value = 0
     dut.uio_in.value = 65
-
+    await ClockCycles(dut.clk, 1)
+    # Set the input values a2=1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 62
+    await ClockCycles(dut.clk, 1)
+    # Set the input values b2=2
+    dut.ui_in.value = 0
+    dut.uio_in.value = 64
    
    
     # Wait for five clock cycle to see the output values
-    await ClockCycles(dut.clk, 5)
+    await ClockCycles(dut.clk, 3)
     assert dut.uo_out.value == 0
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 65
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 64
    
     
 
