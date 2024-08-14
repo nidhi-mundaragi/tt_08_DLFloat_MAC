15c3f4f50a500da1e8152e314d389989da7b3a88
Nidhi M D <166122007+nidhi-mundaragi@users.noreply.github.com>
2024-08-14 11:14:05 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -37,7 +37,7 @@ async def test_project(dut):
     
    #OUTPUT VERIF
     
-    await ClockCycles(dut.clk, 5)
+    await ClockCycles(dut.clk, 4)
     assert dut.uo_out.value == 0
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 65
